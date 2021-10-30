53ad2a222a35baf4a086715a6517a1168fcecca7
Ghous1 <92369426+Ghous1@users.noreply.github.com>
2021-10-31 00:42:24 +0700

    Add files via upload

---
+++ include/core/player/player.cpp
@@ -0,0 +1,10 @@
+#include "player.hpp"
+
+core::board::move bestmove(core::board::position position)
+{
+	core::board::point from;
+	core::board::point to;
+	core::board::move::move _move(from, to);
+	
+	return _move;
+}
