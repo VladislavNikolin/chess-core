e9898c9b956376edb9371303eb519c77f67f51f2
PinkyAcorn <pinkyacorn@gmail.com>
2021-12-07 22:53:55 +0700

    init test

--- test/test.cpp
+++ test/test.cpp
@@ -1,22 +1,6 @@
-#include <iostream>
+#include <gtest/gtest.h>
 
-#include "core/game/game.hpp"
-#include "core/player/ai.hpp"
-#include "core/player/dummy.hpp"
-
-int main(int argc, char const *argv[])
+TEST(CorePlayerAiTest, BasicAssertions)
 {
-    auto white = core::player::ai();
-    auto black = core::player::dummy();
-    auto game = core::game::game();
-
-    while (!game.finished())
-    {
-        game.turn(white.bestmove(game.position()));
-        game.turn(black.bestmove(game.position()));
-    }
-
-    std::cout << "and the winner is... " << (int) game.winner() << std::endl;
-
-    return 0;
+    EXPECT_EQ(5, 5);
 }
