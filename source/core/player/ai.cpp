071f1205faae81baf9a9306dd83338901e88dc99
PinkyAcorn <pinkyacorn@gmail.com>
2021-12-14 20:02:10 +0700

    RLnet Integration: Part 1: The Great Frustration

--- source/core/player/ai.cpp
+++ source/core/player/ai.cpp
@@ -1,10 +1,18 @@
 #include <cstdlib>
+#include <iostream>
+#include <vector>
 
 #include "core/player/ai.hpp"
+
+#include "RLnet.h"
+#include "neural/neuralnetwork.h"
+
+QModel *m = new QModel({10,20,10,2},0.01);
 
 core::board::move core::player::ai::bestmove(core::board::position position)
 {
     auto moves = position.moves();
-    return moves[std::rand() % moves.size()];
+    std::vector<double> legal_moves(2,1);
+    auto res = m->explore(legal_moves);
+    return moves[res % moves.size()];
 }
-
